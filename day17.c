fe3ecfb205d4c75722dd0565d122036969e03773
Akshay Shukla <akshay14yrs@gmail.com>
2026-02-19 04:05:28 +0530

    Update day17.c

--- day17.c
+++ day17.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
-
 int main() {
     int n;
     scanf("%d", &n);
     int arr[n];
-    int visited[n];  
 
-    for(int i = 0; i < n; i++)
-        visited[i] = 0;
-    for(int i = 0; i < n; i++)
-        scanf("%d", &arr[i]);
-  
     for(int i = 0; i < n; i++) {
-        if(visited[i] == 1)
-            continue;
-        int count = 1;
-
-        for(int j = i + 1; j < n; j++) {
-            if(arr[i] == arr[j]) {
-                count++;
-                visited[j] = 1;
-            }
-        }
-        printf("%d:%d ", arr[i], count);
+        scanf("%d", &arr[i]);
     }
+    int max = arr[0];
+    int min = arr[0];
+
+    for(int i = 1; i < n; i++) {
+        if(arr[i] > max)
+            max = arr[i];
+        if(arr[i] < min)
+            min = arr[i];
+    }
+    printf("Max: %d\n", max);
+    printf("Min: %d", min);
     return 0;
 }
